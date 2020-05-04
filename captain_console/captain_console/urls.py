8c4499808a5bf58aae76d1d3dc55ec0b89602e33
Stykki <panoari@gmail.com>
2020-05-04 16:51:23 +0000

    Manufacturer almost

--- captain_console/captain_console/urls.py
+++ captain_console/captain_console/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('captain.urls'))
+    path('', include('captain.urls')),
+    path('manufacturer', include('manufacturer.urls'))
 ]
--- captain_console/manufacturer/views.py
+++ captain_console/manufacturer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from manufacturer.models import Manufacturer
 
 # Create your views here.
+
+def index(request):
+    context = {'manufacturer': Manufacturer.objects.all()}
+    return render(request, 'manufacturer/index.html', context)
