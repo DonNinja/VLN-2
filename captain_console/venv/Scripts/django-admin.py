c69ab8779f0a95dd369d5f664e59a68411e401dd
DonNinja <yngvileo@gmail.com>
2020-05-04 09:52:16 +0000

    Remove venv

--- captain_console/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!E:\Everything\OneDrive\School\HR\Onnur_onn\VLN-2\captain_console\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
