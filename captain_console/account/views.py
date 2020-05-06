6590f96ba2b5c364d27aa2d9fb9bf2f2f2f3f193
Stykki <panoari@gmail.com>
2020-05-06 09:38:41 +0000

    login functionality"

--- captain_console/account/views.py
+++ captain_console/account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'post':
+        pass
+    else:
+        return render(request, 'user/register.html', {
+            'form': UserCreationForm()
+        })
--- captain_console/captain_console/urls.py
+++ captain_console/captain_console/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('captain.urls')),
-    path('manufacturer', include('manufacturer.urls')),
-    path('product', include('product.urls'))
+    path('manufacturer/', include('manufacturer.urls')),
+    path('product/', include('product.urls')),
+    path('account/', include('account.urls'))
 ]
