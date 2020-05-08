fff07adcc69ecaddc0034c1f48750b1c6499ad8c
Magnus Breki <breki.magnusson@gmail.com>
2020-05-08 10:17:54 +0000

    working on edit prof

--- captain_console/account/views.py
+++ captain_console/account/views.py
@@ -26,8 +26,30 @@ def register(request):
         })
 
 def edit_profile(request):
-    if request == 'POST':
-        pass
+    if request.method == 'POST':
+        userN = request.POST['username']
+        passW = request.POST['password1']
+        if len(userN) >= 1:
+            pass
+
+        if len(passW) >= 8:
+            pass
+
+        # print("Chuckles, i'm in danger")
+        # form = UserForm(data=request.POST, instance=request.user)
+        # if form.is_valid():
+
+        #     form.save()
+
+        # user = authenticate(request, username=request.POST['username'], password=request.POST['password1'])
+
+        #     # login(request, user)
+
+        #     profile = Account(name=request.POST['username'], acc_id=request.user)
+        #     profile.save()
+            
+        #     return redirect('profile')
+        
     else:
         current_user = request.user
         user_id = current_user.id
