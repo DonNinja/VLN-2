7a2caf753c4624fcdde5755983c2d64606bc68e9
DonNinja <yngvileo@gmail.com>
2020-05-08 11:56:02 +0000

    Made custom scrollbar c:

--- captain_console/static/js/main.js
+++ captain_console/static/js/main.js
@@ -5,10 +5,13 @@ function fixPos() {
     // $('#footer').height(): Height of the element with id 'footer' from the bottom
     // 38: Additional height from padding, margin and border of footer
     // fix_to_bottom checks if the container and the footer are next to each other (meaning the footer is either at the bottom of the page or you need to scroll to see it) and if it isn't, it adds a padding to container to push the footer to the bottom
-    if (fix_to_bottom !== 0) {
-        $('#container').css('height', fix_to_bottom);
+    $('#container').css('height', fix_to_bottom);
+    /*if (fix_to_bottom !== 0) {
         $('#goUp').hide();
     }
+    else {
+        $('#goUp').show();
+    }*/
 }
 
 
