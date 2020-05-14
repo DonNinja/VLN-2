fed0b91fc483bc34808afc60b76ffe2aaf643d57
Magnus Breki <breki.magnusson@gmail.com>
2020-05-14 14:44:04 +0000

    login required

--- captain_console/histories/views.py
+++ captain_console/histories/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from histories.models import Search_history, Purchase_history
 from account.models import Account
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required()
 def search_index(request):
     """displays users search history"""
     login_id = request.user.id
     context = {'histories': Search_history.objects.all().filter(acc_id=login_id).order_by('-date')}     # get users search history
     return render(request, 'profile/histories/search_history.html', context)
 
+@login_required()
 def purchase_index(request):
     """displays users purchase history"""
     login_id = request.user.id
