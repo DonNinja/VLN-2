b196416ef3315c1df7305c71761a39e3d9fcef1b
Magnus Breki <breki.magnusson@gmail.com>
2020-05-14 10:31:46 +0000

    purchase history

--- captain_console/histories/urls.py
+++ captain_console/histories/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('search_history', views.search_index, name="search_index")
+    path('search_history', views.search_index, name="search_index"),
+    path('purchase_history', views.purchase_index, name="purchase_index")
 ]
--- captain_console/histories/views.py
+++ captain_console/histories/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from histories.models import Search_history
+from histories.models import Search_history, Purchase_history
 from account.models import Account
 
 # Create your views here.
@@ -9,3 +9,9 @@ def search_index(request):
     login_id = request.user.id
     context = {'histories': Search_history.objects.all().filter(acc_id=login_id).order_by('-date')}     # get users search history
     return render(request, 'profile/histories/search_history.html', context)
+
+def purchase_index(request):
+    """displays users purchase history"""
+    login_id = request.user.id
+    context = {'histories': Purchase_history.objects.all().filter(acc_id=login_id).order_by('-date')}     # get users purchase history
+    return render(request, 'profile/histories/purchase_history.html', context)
