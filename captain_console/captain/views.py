977bb9122dc39b2789933a9f64c5dd44f274c417
Magnus Breki <breki.magnusson@gmail.com>
2020-05-14 14:40:42 +0000

    front page functionality

--- captain_console/captain/views.py
+++ captain_console/captain/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from product.models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'captain/index.html')
+    context = {'product': Product.objects.all().filter(on_sale=True)}
+    context['loop_times'] = range(0, len(context['product']))
+    return render(request, 'captain/index.html', context)
